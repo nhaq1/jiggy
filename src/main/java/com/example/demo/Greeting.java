5a82e1dee1f990b96f6a355b990e0673f0efa8c2
nhaq1 <nhaq1@yahoo.com>
2017-07-31 21:05:25 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/Greeting.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class Greeting {
+    private long id;
+    private String content;
+    private String fname;
+    private String lname;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+}
